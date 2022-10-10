7d30f36bae17e121c91df061d2609bc540eb3e9b
Salim <salimbabaji@gmail.com>
2022-10-10 15:18:57 +0100

    done with this

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+    let stringMult = "";
+
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            stringMult += string;
+
+        }
+    }
+
+    else {
+        return "ERROR";
+    }
+
+    return stringMult;
 
 };
 
