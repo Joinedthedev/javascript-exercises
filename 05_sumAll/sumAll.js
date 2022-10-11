59fc3a078cdfe2f6a6585f8a521062c06f236014
Salim <salimbabaji@gmail.com>
2022-10-11 12:20:03 +0100

    done with this one

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+    let z = 0;
+    let error_ = 'ERROR'
 
-};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return error_
+    }
+
+    else if ((a || b) < 0) {
+
+        return error_
+    }
+
+    else if (a > b) {
+        while (a != b - 1) {
+            z += a;
+            a--;
+        }
+    }
+
+    else if (b > a) {
+        while (b != a - 1) {
+            z += b;
+            b--;
+        }
+    }
+
+    return z;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
