999a31a83a3f228b9f639248de7b2225ad6e5c79
Salim <salimbabaji@gmail.com>
2022-10-14 10:11:47 +0100

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = (temp) => {
+  let cTemp = (temp - 32) * (5 / 9)
+  let cRounded = Math.round(cTemp * 10) / 10;
+  
+  return cRounded;
 };
 
-const ctof = function() {
+const ctof = (temp) => {
+  let fTemp = (temp * 1.80) + 32;
+  let fRounded = Math.round(fTemp * 10) / 10;
 
+  return fRounded;
 };
 
 // Do not edit below this line
